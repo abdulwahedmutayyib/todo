1d690d6820675b7c4777f62cb98a2e74976b4095
abdulwahedmutayyib <109347636+abdulwahedmutayyib@users.noreply.github.com>
2024-04-27 12:43:21 +0530

    Update test-todo.py

--- test-todo.py
+++ test-todo.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 from datetime import datetime, timedelta
-from todo_app import ToDoApp
+from todo.py import ToDoApp
 
 class TestToDoApp(unittest.TestCase):
     def setUp(self):
